151e13ddf71d1c0ff5e7940bb664d55226080e44
GikundaMbiriri <petermbiriri8957@gmail.com>
2021-10-27 16:05:21 +0300

    first commit

---
+++ lib/models/category.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Category {
+  final id;
+  final title;
+  final Color color;
+  const Category(
+      {@required this.id, @required this.title, this.color = Colors.orange});
+}
